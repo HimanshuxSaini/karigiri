dcac6ac2832985def96b1ef326113c604cfc7618
Himanshu Saini <himanshusaini07@outlook.com>
2026-09-14 15:31:50 +0530

    feat: add Reel mongoose model

--- server/models/Reel.js
+++ server/models/Reel.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const reelSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  link: { type: String, required: true },
+  tag: { type: String, required: true },
+  handle: { type: String, required: true },
+  url: { type: String },
   image: { type: String },
   order: { type: Number, default: 0 }
 }, { timestamps: true });
